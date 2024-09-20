81df248b919c928032f0ac183d8eea0f504c077b
Dylan-Dev <portaldylan12@gmail.com>
2024-09-19 23:52:03 -0300

    punto 4 hecho

--- TDP/Objetos-Java/TPProyectoAlumnos/TP_ProyectoAlumnos/src/Practicas/Clase1/Punto4.java
+++ TDP/Objetos-Java/TPProyectoAlumnos/TP_ProyectoAlumnos/src/Practicas/Clase1/Punto4.java
@@ -5,7 +5,27 @@ import PaqueteLectura.Lector;
 
 public class Punto4 {
     public static void main(String[] args){
+        GeneradorAleatorio.iniciar();
         
+        int[][] matriz=new int[8][4];
+        int nroPiso,nroOfi;
+        
+        System.out.print("ingrese el numero de piso: ");
+        nroPiso = Lector.leerInt();
+        while(nroPiso != 9){
+            System.out.print("ingrese el numero de oficina: ");
+            nroOfi=Lector.leerInt();
+            matriz[nroPiso-1][nroOfi-1]= matriz[nroPiso-1][nroOfi-1] +1;
+           
+            System.out.print("ingrese el numero de piso: ");
+            nroPiso=Lector.leerInt();    
+        }
+        
+        for(int i=7;i>=0;i--){
+            for(int j=0;j<4;j++){
+                System.out.print(matriz[i][j]+" | ");
+            }
+            System.out.println();
+        }
     }
-    
 }
